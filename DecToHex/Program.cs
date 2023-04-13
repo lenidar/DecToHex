ab753eac4f5a15cf41935be89cf7e7b7e4bc5c88
lenidar <55443074+lenidar@users.noreply.github.com>
2023-04-13 16:36:30 +0800

    Added If Else

--- DecToHex/Program.cs
+++ DecToHex/Program.cs
@@ -26,39 +26,59 @@ namespace DecToHex
             {
                 remainder = numToConvert % 16;
 
-                switch(remainder) 
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        bits.Push(remainder + "");
-                        break;
-                    case 10:
-                        bits.Push("A");
-                        break;
-                    case 11:
-                        bits.Push("B");
-                        break;
-                    case 12:
-                        bits.Push("C");
-                        break;
-                    case 13:
-                        bits.Push("D");
-                        break;
-                    case 14:
-                        bits.Push("E");
-                        break;
-                    case 15:
-                        bits.Push("F");
-                        break;
-                }
+
+                #region Switch-Case
+                //switch (remainder)
+                //{
+                //    case 0:
+                //    case 1:
+                //    case 2:
+                //    case 3:
+                //    case 4:
+                //    case 5:
+                //    case 6:
+                //    case 7:
+                //    case 8:
+                //    case 9:
+                //        bits.Push(remainder + "");
+                //        break;
+                //    case 10:
+                //        bits.Push("A");
+                //        break;
+                //    case 11:
+                //        bits.Push("B");
+                //        break;
+                //    case 12:
+                //        bits.Push("C");
+                //        break;
+                //    case 13:
+                //        bits.Push("D");
+                //        break;
+                //    case 14:
+                //        bits.Push("E");
+                //        break;
+                //    case 15:
+                //        bits.Push("F");
+                //        break;
+                //} 
+                #endregion
+
+                #region if-Else
+                if(remainder >= 0 && remainder <= 9)
+                    bits.Push(remainder + "");
+                else if(remainder == 10)
+                    bits.Push("A");
+                else if (remainder == 11)
+                    bits.Push("B");
+                else if (remainder == 12)
+                    bits.Push("C");
+                else if (remainder == 13)
+                    bits.Push("D");
+                else if (remainder == 14)
+                    bits.Push("E");
+                else if (remainder == 15) 
+                    bits.Push("F");
+                #endregion
 
                 numToConvert = numToConvert - remainder;
                 numToConvert = numToConvert / 16;
